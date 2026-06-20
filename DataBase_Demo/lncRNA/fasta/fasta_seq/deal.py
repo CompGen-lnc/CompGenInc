8e66501f1885ccaf8f75171f93aab768b2b25aee
ccalderon13 <svkh13@gmail.com>
2026-06-19 19:10:58 -0600

    data_9

---
+++ DataBase_Demo/lncRNA/fasta/fasta_seq/deal.py
@@ -0,0 +1,28 @@
+from Bio import SeqIO
+
+def fix_seq(seq):
+    seq = seq.replace('t', 'T')
+    seq = seq.replace('a', 'A')
+    seq = seq.replace('c', 'C')
+    seq = seq.replace('g', 'G')
+    seq = seq.replace('u', 'T')
+    seq = seq.replace('U', 'T')
+
+    return seq
+
+def read_fa(path):
+    res = {}
+    rescords = list(SeqIO.parse(path, format="fasta"))
+    for x in rescords:
+        id = str(x.id)
+        seq = fix_seq(str(x.seq))
+        res[id] = seq
+    return res
+
+lncrna = read_fa("/Users/andrescubillo/Documents/Demo/data/fasta/fasta_seq/homo_mature_mirna.fa")
+
+f = open("outLncRNA_new.fas","w")
+for k,v in lncrna.items():
+    f.write(">"+k+"\n")
+    f.write(v+"\n")
+f.close()
